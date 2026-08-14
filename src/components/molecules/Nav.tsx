1aeeae58920898fb7591572e7122b60d7a350ef2
James Usher <jamesusher1633@gmail.com>
2026-08-13 22:19:42 -0500

    Add "home" to top nav

--- src/components/molecules/Nav.tsx
+++ src/components/molecules/Nav.tsx
@@ -1,13 +1,7 @@
 import { useState } from 'react'
 import NavLink from '../atoms/NavLink'
 import MenuToggle from '../atoms/MenuToggle'
-
-const links = [
-  { href: '#about', label: 'About' },
-  { href: '#case-studies', label: 'Case Studies' },
-  { href: '#code-samples', label: 'Code Samples' },
-  { href: '#contact', label: 'Contact' },
-]
+import { sections } from '../../content/navigation'
 
 function Nav() {
   const [isOpen, setIsOpen] = useState(false)
@@ -16,10 +10,10 @@ function Nav() {
     <nav>
       <MenuToggle isOpen={isOpen} onClick={() => setIsOpen((open) => !open)} />
       <ul id="site-nav-list" className={isOpen ? 'open' : undefined}>
-        {links.map((link) => (
-          <li key={link.href}>
-            <NavLink href={link.href} onClick={() => setIsOpen(false)}>
-              {link.label}
+        {sections.map((section) => (
+          <li key={section.id}>
+            <NavLink href={`#${section.id}`} onClick={() => setIsOpen(false)}>
+              {section.label}
             </NavLink>
           </li>
         ))}
