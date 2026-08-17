64cfe2cbb9b4367511250014844a9fdf20bab673
James Usher <jamesusher1633@gmail.com>
2026-08-17 01:17:19 -0500

    Add watermark to all pages except first
    cosmetic improvement

--- src/components/organisms/About.tsx
+++ src/components/organisms/About.tsx
@@ -1,14 +1,19 @@
 import MediaText from '../molecules/MediaText'
 import Heading from '../atoms/Heading'
+import SectionWatermark from '../atoms/SectionWatermark'
 import useOverscrollBump from '../../hooks/useOverscrollBump'
 import { about, siteContent } from '../../content/site'
+import { sections } from '../../content/navigation'
 import profilePhoto from '../../assets/profile.png'
+
+const watermarkText = sections.find((section) => section.id === 'about')!.label
 
 function About() {
   const bumpRef = useOverscrollBump<HTMLElement>()
 
   return (
     <section id="about" ref={bumpRef}>
+      <SectionWatermark text={watermarkText} />
       <MediaText imageSrc={profilePhoto} imageAlt={siteContent.name}>
         <Heading>{about.intro}</Heading>
         {about.paragraphs.map((paragraph, index) => (
