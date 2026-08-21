f6f1f416b154e73d14a1678291c8fee3d5c5ed52
James Usher lap <jamesusher1633@gmail.com>
2026-08-21 18:30:55 -0500

    Place about text in a box similar to experience/CS

--- src/components/organisms/About/About.tsx
+++ src/components/organisms/About/About.tsx
@@ -1,5 +1,6 @@
 import MediaText from '../../molecules/MediaText/MediaText'
 import Heading from '../../atoms/Heading/Heading'
+import TextCard from '../../molecules/TextCard/TextCard'
 import useOverscrollBump from '../../../hooks/useOverscrollBump'
 import { about, siteContent } from '../../../content/site'
 import profilePhoto from '../../../assets/profile.png'
@@ -9,13 +10,15 @@ function About() {
   const bumpRef = useOverscrollBump<HTMLElement>()
 
   return (
-    <section id="about" ref={bumpRef}>
-      <MediaText imageSrc={profilePhoto} imageAlt={siteContent.name} contentClassName={styles.content}>
-        <Heading>{about.intro}</Heading>
-        {about.paragraphs.map((paragraph, index) => (
-          <p key={index}>{paragraph}</p>
-        ))}
-      </MediaText>
+    <section id="about" aria-label={sectionLabel} ref={bumpRef}>
+      <div className={styles.content}>
+        <TextCard>
+          <Heading>{about.intro}</Heading>
+          {about.paragraphs.map((paragraph, index) => (
+            <p key={index}>{paragraph}</p>
+          ))}
+        </TextCard>
+      </div>
     </section>
   )
 }
