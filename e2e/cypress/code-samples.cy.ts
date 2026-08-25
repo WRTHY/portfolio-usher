0e30876991610b114c03e807c3569a7dc1d040f1
James Usher lap <jamesusher1633@gmail.com>
2026-08-25 00:15:40 -0500

    Reorder testing specs

--- e2e/cypress/code-samples.cy.ts
+++ e2e/cypress/code-samples.cy.ts
@@ -10,25 +10,24 @@ describe('automation examples panel', () => {
   it('switching framework changes the file tabs and visible code', () => {
     const { codeSamples } = portfolioPage
 
-    codeSamples.fileTab('case-study-modal.spec.ts').should('be.visible')
-    codeSamples.activeCodePanel.should('include.text', "from '@playwright/test'")
+    codeSamples.fileTab('case-studies.spec.ts').should('be.visible')
+    codeSamples.activeCodePanel.should('include.text', "from './fixtures'")
 
     codeSamples.selectFramework('cypress')
 
-    codeSamples.fileTab('case-study-modal.cy.ts').should('be.visible')
-    codeSamples.fileTab('case-study-modal.spec.ts').should('not.exist')
-    codeSamples.activeCodePanel.should('include.text', "describe('Portfolio")
+    codeSamples.fileTab('case-studies.cy.ts').should('be.visible')
+    codeSamples.activeCodePanel.should('include.text', 'new PortfolioPage()')
   })
 
   it('switching file tabs changes the visible code and file path', () => {
     const { codeSamples } = portfolioPage
 
-    codeSamples.activeFilePath.should('have.text', 'playwright/case-study-modal.spec.ts')
+    codeSamples.activeFilePath.should('have.text', 'playwright/case-studies.spec.ts')
 
-    codeSamples.selectFile('fixtures.ts')
+    codeSamples.selectFile('playwright.config.ts')
 
-    codeSamples.activeFilePath.should('have.text', 'playwright/fixtures.ts')
-    codeSamples.activeCodePanel.should('include.text', 'export async function openCaseStudy')
+    codeSamples.activeFilePath.should('have.text', 'playwright.config.ts')
+    codeSamples.activeCodePanel.should('include.text', 'defineConfig')
   })
 
   it('the Performance testing type is disabled and non-interactive', () => {
